0538666465eb6a8c35f653ae5c2601a32a12ef3c
Vlada Antonchenko <vladajka16@gmail.com>
2020-08-05 16:23:25 -0400

    commit2

---
+++ Develop/README.md
@@ -0,0 +1,48 @@
+ 
+ 
+![Avatar](https://avatars1.githubusercontent.com/u/60941060?v=4)
+ 
+User Name: vlada2020
+ 
+Email: null
+
+
+![bage image](https://img.shields.io/static/v1?label=Version&message=active&color=<COLOR>) 
+## Project Title:
+
+ # good README generator 
+ 
+## Description: 
+
+ sssssss 
+ 
+## Table of contents
+ 
+### Installation
+
+ npm instal
+ 
+
+### Usage
+ 
+ sssssss
+ 
+### Credits 
+ 
+ ssssss
+ 
+### License 
+ 
+ ssssss
+ 
+## Contributing 
+ 
+ vlada antonchenko
+ 
+## Tests 
+ 
+ manual test
+ 
+## Questions 
+ 
+ no questions
--- Develop/index.js
+++ Develop/index.js
@@ -1,12 +1,145 @@
-const questions = [
+const fs = require("fs");
+const axios = require("axios");
+const inquirer = require("inquirer");
 
-];
+inquirer
+	.prompt({
+	  message: "Enter your GitHub username:",
+      name: "username", 
+      type:"input",
+	})
+	.then(function ({ username }) {
+		const queryUrl = `https://api.github.com/users/${username}`;
 
-function writeToFile(fileName, data) {
-}
+		axios.get(queryUrl).then(function (res) {
+			// console.log(res.data);
 
-function init() {
+			// Get Username
+			const username = res.data.login;
 
-}
+			// Get Avatar Image
+			const avatarURL = res.data.avatar_url;
+			const avatar = '![Avatar](' + avatarURL + ')';
 
-init();
+			// Get Email
+			const email = res.data.email;
+
+			let document = 
+			// Avatar
+			' \n \n' +
+			avatar + '\n \n' +
+			// Usermame
+			'User Name: ' + username + '\n \n' +
+			// Email
+			'Email: ' + email + '\n';
+
+			fs.writeFile("README.md", document, function (err) {
+				if (err) {
+					throw err;
+				}
+			});
+		})
+
+		.then(function(){
+			inquirer.prompt([
+				{
+					message:"What is your badge version? ",
+					name: "badge",
+					type:"input"
+				},
+				{
+					message:"What is the project title? ",
+					name: "project",
+					type:"input"
+				},
+				{
+					message:"What is your project description? ",
+					name: "description",
+					type:"input"
+				},
+				{
+					message:"Table of contents are as follows(press enter)? ",
+					name: "table",
+					//type:"input"
+				},
+				{
+					message:"installation? ",
+					name: "install",
+					type:"input"
+                },
+                {
+					message:"usages ? ",
+					name: "usage",
+					type:"input"
+				},
+                
+                {
+					message:"credits ? ",
+					name: "credit",
+					type:"input"
+				},
+				 {
+					message:" license ? ",
+					name: "license",
+					type:"input"
+				},
+				
+				{
+					message:"Who is contributing? ",
+					name: "contribute",
+					type:"input"
+				},
+				{
+					message:"What are the required tests? ",
+					name: "test",
+					type:"input"
+                },
+                {
+                    message:"what is the questions?",
+                    name:"questions",
+                    type:"input"
+                }
+			])
+			.then(function(answers){
+				console.log(answers);
+
+				//let userQs = 
+				// Prompt second set of questions
+				//`## Second Set of Questions  \n \n`
+	
+				let body2 =
+
+				// Badge
+				`\n\n![bage image](https://img.shields.io/static/v1?label=Version&message=${answers.badge}&color=<COLOR>) \n`+
+
+				// Project Title
+				`## Project Title:\n\n # ${answers.project} \n \n`+
+
+				// Project Description
+                `## Description: \n\n ${answers.description} \n \n`+
+                //table of content
+                `## Table of contents\n \n`+//${answers.table}\n \n`+
+                //installation
+                `### Installation\n\n ${answers.install}\n \n\n`+
+                //usage
+                `### Usage\n \n ${answers.usage}\n \n`+
+                //Credits
+                `### Credits \n \n ${answers.credit}\n \n`+
+                 //License
+                `### License \n \n ${answers.license}\n \n`+
+                //contributing
+                `## Contributing \n \n ${answers.contribute}\n \n`+
+                //tests
+                `## Tests \n \n ${answers.test}\n \n`+
+                //questions
+                `## Questions \n \n ${answers.questions}`
+                
+				var document =  body2;
+				fs.appendFile("README.md", document, function (err) {
+					if (err) {
+						throw err;
+					}
+				});
+			})
+		})
+	})
